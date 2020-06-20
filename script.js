8b65677dca37b1f00952573211b86f977e822b0d
Robert Luttig <robert.luttig@gmail.com>
2020-06-20 13:23:39 -0700

    added a moment function to color code update current hours

--- script.js
+++ script.js
@@ -8,7 +8,23 @@ $(function () {
     function updateTime() {
       var dateEl = $("#currentDay");
       var now = moment();
-      var formattedDate = now.format("dddd MMMM do");
+      var formattedDate = now.format("dddd MMMM Do");
       dateEl.text(formattedDate);
     }
   });
+
+//time coding to reflect current hour
+
+  var currentHour = moment().hour();
+$(".time-block").each(function () {
+  var id = parseInt($(this).attr("id"));
+  if (id === currentHour) {
+    $(this).attr("class", "row time-block present");
+  } else if (id < currentHour) {
+    $(this).attr("class", "row time-block past");
+  } else if (id > currentHour) {
+    $(this).attr("class", "row time-block future");
+  }
+});
+
+//save user input
